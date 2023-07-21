beaf427a90982a9eb4514e0d053f465cb72f0285
JebaSolomonRajS <mynewworkmail007@gmail.com>
2023-07-21 16:10:58 +0530

    favorite and favorite list

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import useMoviesList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
 
 // Uncomment if you plan to use the Inter font
 // const inter = Inter({ subsets: ["latin"] });
@@ -30,6 +31,7 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: movies = [] } = useMoviesList();
+  const { data: favorites = [] } = useFavorites();
 
   return (
     <>
@@ -37,6 +39,7 @@ export default function Home() {
       <Billboard />
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </>
   );
