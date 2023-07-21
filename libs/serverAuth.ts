bb522933c9001f55e12bfd951f29c4aa9bb0c58d
JebaSolomonRajS <mynewworkmail007@gmail.com>
2023-07-21 17:37:22 +0530

    get session update

--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth";
 
 import prismadb from "@/libs/prismadb";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
