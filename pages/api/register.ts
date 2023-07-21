3bd8883da750c92341b435a211f9822c6372781a
JebaSolomonRajS <mynewworkmail007@gmail.com>
2023-07-21 17:42:47 +0530

    register updates

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -6,22 +6,25 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).end();
-  }
-
   try {
+    if (req.method !== "POST") {
+      return res.status(405).end();
+    }
+
     const { email, name, password } = req.body;
+
     const existingUser = await prismadb.user.findUnique({
       where: {
         email,
       },
     });
+
     if (existingUser) {
       return res.status(422).json({ error: "Email taken" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
+
     const user = await prismadb.user.create({
       data: {
         email,
@@ -31,9 +34,9 @@ export default async function handler(
         emailVerified: new Date(),
       },
     });
+
     return res.status(200).json(user);
   } catch (error) {
-    console.log(error);
-    return res.status(400).end();
+    return res.status(400).json({ error: `Something went wrong: ${error}` });
   }
 }
